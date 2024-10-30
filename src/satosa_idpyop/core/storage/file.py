da042d5aad8cf9369603eaa0e6eee5c8db6ab7b2
Roland Hedberg <roland@catalogix.se>
2024-10-30 09:50:35 +0100

    Added NO cache file storage class

--- src/satosa_idpyop/core/storage/file.py
+++ src/satosa_idpyop/core/storage/file.py
@@ -2,6 +2,7 @@ import logging
 from typing import Optional
 
 from idpyoidc.storage.abfile import AbstractFileSystem
+from idpyoidc.storage.abfile_no_cache import AbstractFileSystemNoCache
 
 from .base import Storage
 
@@ -20,6 +21,41 @@ class FilesystemDB(AbstractFileSystem, Storage):
             **kwargs
     ):
         AbstractFileSystem.__init__(self, fdir, key_conv, value_conv)
+
+    def fetch(self, information_type: str, key: Optional[str] = ""):
+        if VERBOSE:
+            logger.debug(f"Fetching {information_type}{DIVIDER}{key} from persistent storage")
+        if key:
+            return self.get(DIVIDER.join([information_type, key]))
+        else:
+            return self.get(information_type)
+
+    def store(self, information_type: str, value, key: Optional[str] = ""):
+        if VERBOSE:
+            logger.debug(f"Storing {information_type}{DIVIDER}{key} to persistent storage")
+        if key:
+            self[DIVIDER.join([information_type, key])] = value
+        else:
+            self[information_type] = value
+
+    def keys_by_information_type(self, information_type: str):
+        itype = f"{information_type}:"
+        return [k.split(DIVIDER)[1] for k in self.keys() if k.startswith(itype)]
+
+    def information_types(self):
+        return {k.split(DIVIDER)[0] for k in self.keys()}
+
+
+class FilesystemDBNoCache(AbstractFileSystemNoCache, Storage):
+
+    def __init__(
+            self,
+            fdir: Optional[str] = "",
+            key_conv: Optional[str] = None,
+            value_conv: Optional[str] = None,
+            **kwargs
+    ):
+        AbstractFileSystemNoCache.__init__(self, fdir, key_conv, value_conv)
 
     def fetch(self, information_type: str, key: Optional[str] = ""):
         if VERBOSE:
